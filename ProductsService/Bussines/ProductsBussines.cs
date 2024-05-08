fe2983d57d8046a3a810cd3ead83afd043fb9725
Edward Martinez <Alejed@gmail.com>
2024-05-08 18:36:01 -0500

    Commit Inicial

---
+++ ProductsService/Bussines/ProductsBussines.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopifySharp;
+using System.Configuration;
+using ShopifySharp.Filters;
+using ShopifySharp.Infrastructure;
+using ShopifySharp.Lists;
+using ShopifySharp.Utilities;
+
+namespace ProductsService.Bussines
+{
+    public class ProductsBussines
+    {
+        string shopifyUrl = string.Empty;
+        string apiKey = string.Empty;
+        public ProductsBussines()
+        {
+            this.shopifyUrl = ConfigurationManager.AppSettings["ShopifyUrl"];
+            this.apiKey = ConfigurationManager.AppSettings["ApiKey"];
+        }
+
+        public async Task<string> getArticleListAsync()
+        {
+            var service = new BlogService(shopifyUrl, apiKey);
+            var blogs = await service.ListAsync();
+
+            var serviceProduct = new ProductService(shopifyUrl, apiKey);
+            int productCount = await serviceProduct.CountAsync();
+
+
+            var serviceList = new ProductService(shopifyUrl, apiKey);
+            var products = await serviceList.ListAsync();
+
+            //Optionally filter the results
+            //var filter = new ProductFilterOptions()
+            //{
+            //    Ids = new[]
+            //    {
+            //        productId1,
+            //        productId2,
+            //        productId3
+            //    }
+            //};
+            //products = await serviceList.ListAsync(filter);
+
+            return string.Empty;
+        }
+
+    }
+}
---
+++ ProductsService/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProductsService.Bussines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProductsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new Service1()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+
+            ProductsBussines productsBussines = new ProductsBussines();
+            productsBussines.getArticleListAsync().Wait();
+        }
+    }
+}
